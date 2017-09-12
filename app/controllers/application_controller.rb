ccd2e7a15ca10ab0b647223f46f3c26830f1029f
Ricardo Martinez <Ricardo.Martinez@Mckesson.com>
2017-09-12 20:30:07 +0000

    update homepage show hostname

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :gethost
+  
+  def gethost
+    @hostname = ENV['HOSTNAME'] || 'Anonymous'
+    
+    
+  end
 end
